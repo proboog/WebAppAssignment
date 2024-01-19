56ee1e780ca5294c8411f90cedb26eb30e1b4eb9
unknown <f4himanwar@gmail.com>
2024-01-19 19:02:43 +0800

    modified the login, register and homepage nav bar

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ Don't forget ENV and Vendor file.
 Vendor File:https://www.mediafire.com/file/9pwllyv7tnpkmp1/vendor.zip/file
 
 ENV File:https://www.mediafire.com/file/f8ys6h4fcf5yrog/env.rar/file
+
+
+These are the steps to run the program after cloning the project.
+
+Open terminal and type : 
+1. composer update
+2. cp .env.example .env
+3. php artisan key:generate
+4. php artisan serve
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
-<html>
-<title>W3.CSS</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<body>
-<div class="w3-top">
-  <div class="w3-bar w3-theme w3-top w3-left-align w3-large">
-    <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hover-white w3-large w3-theme-l1" href="javascript:void(0)" onclick="w3_open()"><i class="fa fa-bars"></i></a>
-    <a href="#" class="w3-bar-item w3-button w3-theme-l1">Logo</a>
-    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-white">About</a>
-    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Log in</a>
-    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Sign up</a>
-    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Contact</a>
-  </div>
-</div>
+@extends('layouts.template')
+
+@section('body')
+
+<title>Sign Up Page</title>
 
 <header class="w3-container w3-teal w3-padding-top-32">
   <h1>Register Account</h1>
@@ -39,6 +25,8 @@
 <p>
 <label>Gender</label></p>
 <input class="w3-radio" type="radio" name="gender" value="male" checked>
+
+
 <label>Male</label></p>
 
 <p>
@@ -51,6 +39,5 @@
 </form>
 
 </div>
-
-</body>
-</html> 
+    
+@endsection
