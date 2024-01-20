93101474ad70cef864bfa87c222fd3d59029b1a9
unknown <f4himanwar@gmail.com>
2024-01-21 03:01:10 +0800

    update views with log in function

---
+++ resources/views/register.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<body>
+<div class="w3-top">
+  <div class="w3-bar w3-theme w3-top w3-left-align w3-large">
+    <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hover-white w3-large w3-theme-l1" href="javascript:void(0)" onclick="w3_open()"><i class="fa fa-bars"></i></a>
+    <a href="#" class="w3-bar-item w3-button w3-theme-l1">Logo</a>
+    <a href="home" class="w3-bar-item w3-button w3-hide-small w3-hover-white">About</a>
+    <a href="login" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Log in</a>
+    <a href="register" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Sign up</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Contact</a>
+  </div>
+</div>
+
+<header class="w3-container w3-teal w3-padding-top-32">
+  <h1>Register Account</h1>
+</header>
+
+<div class="w3-container w3-half w3-margin-top">
+
+<form class="w3-container w3-card-4">
+
+<p>
+<label>Email</label></p>
+<input class="w3-input" type="text" style="width:90%" required>
+<p>
+<label>Password</label></p>
+<input class="w3-input" type="password" style="width:90%" required>
+<p>
+<label>Retype password</label></p>
+<input class="w3-input" type="password" style="width:90%" required>
+
+<p>
+<label>Gender</label></p>
+<input class="w3-radio" type="radio" name="gender" value="male" checked>
+<label>Male</label></p>
+
+<p>
+<input class="w3-radio" type="radio" name="gender" value="female">
+<label>Female</label></p>
+
+<p>
+<button class="w3-button w3-section w3-teal w3-ripple"> Register account </button></p>
+
+</form>
+
+</div>
+
+</body>
+</html> 
