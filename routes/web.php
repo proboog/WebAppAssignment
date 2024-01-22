26317b15839693f57957ce5e1b95b464d22bda52
megat <1201100484@student.mmu.edu.my>
2024-01-23 06:05:14 +0800

    admin route

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('createEvent', function () {
     return view('createEvent');
 });
+
+Route::get('/adminPage', function () {
+    return view('adminPage');
+});
