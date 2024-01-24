39af394bf028a40a06afff7f740c259a42354e1e
unknown <f4himanwar@gmail.com>
2024-01-24 16:05:58 +0800

    added migration file

---
+++ database/migrations/2024_01_24_053552_create_events_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id('Event_ID');
+            $table->string('Event_name');
+            $table->string('Event_description');
+            $table->string('Type_of_event');
+            $table->string('Date_and_time');
+            $table->string('Venue');
+            $table->string('Organizer_name');
+            $table->string('Contact');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
