60dbdf5f081450575f6ed4f4979134f50247d097
megat <1201100484@student.mmu.edu.my>
2024-01-26 13:18:40 +0800

    new home

---
+++ resources/views/eventManage.blade.php
@@ -0,0 +1,79 @@
+<!-- eventManage.blade.php -->
+
+<!DOCTYPE html>
+<html>
+<title>Event Management</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<body>
+    <style>
+        html,
+        body,
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+            font-family: "Roboto", sans-serif;
+        }
+
+        .w3-sidebar {
+            z-index: 3;
+            width: 250px;
+            top: 43px;
+            bottom: 0;
+            height: inherit;
+        }
+    </style>
+    <div class="w3-top">
+        <div class="w3-bar w3-theme w3-top w3-left-align w3-large">
+            <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hover-white w3-large w3-theme-l1" href="javascript:void(0)" onclick="w3_open()"><i class="fa fa-bars"></i></a>
+            <a href="#" class="w3-bar-item w3-button w3-theme-l1">Logo</a>
+            <a href="/" class="w3-bar-item w3-button w3-hide-small w3-hover-white">About</a>
+            <a href="{{ route('login') }}" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Log in</a>
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Sign up</a>
+            @endif
+
+            <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Contact</a>
+        </div>
+    </div>
+
+    <div class="w3-container w3-half w3-margin-top">
+        <header class="w3-container w3-teal w3-padding-top-32">
+            <h1>Delete/Edit Event</h1>
+        </header>
+
+        <form action="{{ route('storeEvent')}}" class="w3-container w3-card-4" method="POST">
+            @csrf
+            <p>
+                <label>Select Event:</label>
+                <select class="w3-select" name="selectedEvent">
+                    <!-- Populate this dropdown with events dynamically from your backend -->
+                    <option value="event1">Event 1</option>
+                    <option value="event2">Event 2</option>
+                    <option value="event3">Event 3</option>
+                </select>
+            </p>
+
+            <p>
+                <label>Action:</label>
+                <select class="w3-select" name="action">
+                    <option value="delete">Delete</option>
+                    <option value="edit">Edit Description</option>
+                </select>
+            </p>
+
+            <p>
+                <button class="w3-button w3-section w3-red w3-ripple" type="submit">  Perform Action </button>
+            </p>
+        </form>
+    </div>
+</body>
+
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,58 +4,119 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>User dashboard</title>
+    <title>User Profile</title>
+    <style>
+        body {
+            font-family: 'Roboto', sans-serif;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
 
+        .w3-top {
+            width: 100%;
+            z-index: 1;
+        }
 
+        .w3-navbar {
+            background-color: #333;
+            color: #fff;
+            padding: 1px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            width: 100%;
+            box-sizing: border-box;
+            margin-bottom: 0; /* Remove margin bottom */
+        }
+
+        .w3-container {
+            width: 100%;
+            text-align: left;
+            margin-top: 0;
+        }
+
+        .username-container {
+            margin-left: 20px;
+        }
+
+        .logout-button {
+            background-color: #555;
+            color: #fff;
+            padding: 5px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .logout-button:hover {
+            background-color: #777;
+        }
+
+        .message-container {
+            margin: 20px auto;
+            padding: 10px;
+            border: 3px solid #555;
+            border-radius: 5px;
+            width: 60%;
+            box-sizing: border-box;
+        }
+
+        .data-label {
+            font-weight: bold;
+            margin-top: 15px;
+            margin-bottom: 5px;
+        }
+
+        .data-value {
+            margin-bottom: 15px;
+            border-top: 1px solid #555;
+            padding-top: 10px;
+        }
+    </style>
 </head>
+
 <body>
 
-
     <div class="w3-top">
-        <div class="w3-bar w3-theme w3-top w3-left-align w3-large">
-
-            <div class="w3-bar-item w3-button w3-right w3-hide-small w3-hover-white">{{ Auth::user()->name }}</option></div>   
-
+        <div class="w3-navbar w3-theme w3-left-align w3-large">
+            <div class="username-container">
+                <p>{{ Auth::user()->name }}</p>
+            </div>
+            <button class="logout-button" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                Logout
+            </button>
         </div>
 
-
-
-
+        <header class="w3-container w3-teal">
+            <h1>User Profile</h1>
+        </header>
     </div>
 
-    <br><br><br>
-
-    <p>Hey {{ Auth::user()->name }}. We are working on this page !</p>
     
+
+    <div class="message-container">
+        <div class="data-label">Username</div>
+        <div class="data-value">{{ Auth::user()->name }}</div>
+
+        <div class="data-label">Email</div>
+        <div class="data-value">{{ Auth::user()->email }}</div>
+
+        <div class="data-label">Date Joined</div>
+        <div class="data-value">{{ Auth::user()->created_at }}</div>
+    </div>
+
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+        @csrf
+    </form>
 </body>
 </html>
-
-
-
-<li class="nav-item dropdown">
-    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-        {{ Auth::user()->name }}
-    </a>
-
-    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item" href="{{ route('logout') }}"
-           onclick="event.preventDefault();
-                         document.getElementById('logout-form').submit();">
-            {{ __('Logout') }}
-        </a>
-
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-            @csrf
-        </form>
-    </div>
-</li>
-
-
